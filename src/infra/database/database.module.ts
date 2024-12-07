805740c34b34ccda490c4a836c33b81a829844af
siranweb <siranweb.public@tuta.io>
2024-12-07 12:38:26 +0300

    fix: di constants

--- src/infra/database/database.module.ts
+++ src/infra/database/database.module.ts
@@ -1,11 +1,8 @@
 import { Inject, Module, OnApplicationShutdown } from '@nestjs/common';
 import { ConfigModule } from '@/infra/config/config.module';
-import {
-  DATABASE_DI_CONSTANTS,
-  providers,
-  publicProviders,
-} from '@/infra/database/database.providers';
+import { providers, publicProviders } from '@/infra/database/database.providers';
 import { IDatabase } from '@/infra/database/types/database.interface';
+import { DATABASE_DI_CONSTANTS } from '@/infra/database/database.di-constants';
 
 @Module({
   imports: [ConfigModule],
